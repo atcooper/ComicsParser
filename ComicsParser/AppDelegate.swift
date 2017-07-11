170a219477d6716973ba6bfe2c8d6aa577b973a2
Anthony Cooper <anthonycooper@gmail.com>
2017-07-11 14:46:02 -0500

    Initial Commit

---
+++ ComicsParser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ComicsParser
+//
+//  Created by Anthony Cooper on 7/11/17.
+//  Copyright © 2017 Anthony Cooper. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ComicsParser/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ComicsParser
+//
+//  Created by Anthony Cooper on 7/11/17.
+//  Copyright © 2017 Anthony Cooper. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
